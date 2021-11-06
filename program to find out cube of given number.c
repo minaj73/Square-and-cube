879368038e0e4879a42103e56ba8e8f0911175b9
minaj73 <93118731+minaj73@users.noreply.github.com>
2021-11-07 00:46:06 +0530

    Add files via upload

---
+++ program to find out cube of given number.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int no=0, cube=1;
+    printf("\n enter number ");
+scanf("%d",&no);
+cube=no*no*no;
+printf("\n cube of %d is = %d",no,cube);
+    getch();
+    return 0;
+}
---
+++ program to find out square of given number.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int no=0, sqr=1;
+    printf("\n enter number ");
+scanf("%d",&no);
+sqr=no*no;
+printf("\n square of %d is = %d",no,sqr);
+    getch();
+    return 0;
+}
